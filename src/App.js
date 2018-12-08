4724ca6b7dfd8052a7797b6da90180185202233e
Enver Selchuk <enver.selchuk@kier.co.uk>
2018-12-08 15:17:17 +0000

    removed modal and fixed file root errors

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import MovieBrowser from './modules/movie-browser/movie-browser.container';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // Provides the Material UI theme to child components
+      <MuiThemeProvider>
+        <MovieBrowser />
+      </MuiThemeProvider>
     );
   }
 }
 
 export default App;
+
+  
